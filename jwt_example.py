bf741554a388465615bdf58bc106863d9e804d92
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-05-15 10:48:19 +0400

    !

--- jwt_example.py
+++ jwt_example.py
@@ -12,19 +12,47 @@ JWT — специальный токен для авторизации.
 import jwt
 import datetime
 
-SECRET_KEY = "supersecret"
+SECRET_KEY = "supersecret" # Это ключ, которым подписывается токен. 
+# С его помощью сервер: создаёт токен, потом проверяет, что токен не подделан
+# Если злоумышленник узнает этот ключ — он сможет создавать свои токены.
+# Поэтому в реальных проектах: ключ длинный, сложный, хранится в .env
 
-payload = {
+payload = {                     
     "user_id": 1,
-    "exp": datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)
+    "exp": datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1) 
 }
-token = jwt.encode(
+"""
+Payload — данные внутри токена
+JWT состоит из: Header, Payload, Signature
+Payload — это полезные данные.
+"user_id": 1 - означает, что токен принадлежит пользователю с id = 1
+"exp" — expiration time - Тут создаётся время окончания действия токена.
+
+datetime.datetime.utcnow() - Получаем текущее время UTC.
+UTC используется потому что: это мировой стандарт, нет проблем с часовыми поясами
+
+datetime.timedelta(hours=1) - Создаёт промежуток времени 1 час
+
+Можно так:
+
+datetime.timedelta(days=7)
+datetime.timedelta(minutes=30)
+Сложение
+datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+
+означает:
+
+текущее время + 1 час
+
+То есть токен станет недействительным через час.
+"""
+token = jwt.encode(    # jwt.encode() кодирует данные в JWT.
     payload,
     SECRET_KEY,
-    algorithm="HS256"
+    algorithm="HS256"  # Алгоритм шифрования/подписи. HS256: самый популярный, использует секретный ключ
 )
 
-print(f"Encoded {token}")
+print(f"ENCODED {token}")
 
 # Расшифровка токена
 data = jwt.decode(
@@ -33,4 +61,8 @@ data = jwt.decode(
     algorithms=["HS256"]
 )
 
-print(f"Decoded {data}")
+print(f"DECODED {data}")
+
+# Что хранится внутри JWT
+
+
